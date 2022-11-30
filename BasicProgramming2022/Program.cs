f051dff05a6382ee80d93414415c71866b83b8a7
amansoni <amansoni2041999@gmail.com>
2022-12-01 00:13:53 +0530

    performing poweroftwo

--- BasicProgramming2022/Program.cs
+++ BasicProgramming2022/Program.cs
@@ -11,25 +11,20 @@ namespace BasicProgramming2022
         static void Main(string[] args)
         {
 
-            int chk_year;
+            double result;
+            Console.WriteLine("Enter The Power");
+            int Power = int.Parse(Console.ReadLine());
+            int a = 2;
+            for (int i = 0; i <= Power; i++)
+            {
+                result = Math.Pow(a, i);
+                Console.WriteLine(a + " power of " + i + " is " + result);
+                Console.ReadLine();
+            }
 
-            Console.Write("Check whether a given year is leap year or not : ");
-
-
-            Console.WriteLine("Input an year : ");
-            chk_year = Convert.ToInt32(Console.ReadLine());
-
-            if ((chk_year % 400) == 0)
-                Console.WriteLine("{0} is a leap year : ", chk_year);
-            else if ((chk_year % 100) == 0)
-                Console.WriteLine("{0} is not a leap year : ", chk_year);
-            else if ((chk_year % 4) == 0)
-                Console.WriteLine("{0} is a leap year : ", chk_year);
-            else
-                Console.WriteLine("{0} is not a leap year : ", chk_year);
-            Console.ReadLine();
         }
-
-        
     }
-}
+}   
+          
+    
+
